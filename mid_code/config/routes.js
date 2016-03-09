d5713898d842096e1cd44f14f2979bfb94e937e6
h4w5 <pj@ga.co>
2016-03-08 20:03:12 -0800

    move todos to public/todos.html in mid_code

--- mid_code/config/routes.js
+++ mid_code/config/routes.js
@@ -7,6 +7,5 @@ var pagesController = require('../controllers/pages');
 // root path:
 router.get('/',      pagesController.home);
 router.get('/about', pagesController.about);
-router.get('/todos', pagesController.todos);
 
 module.exports = router;
